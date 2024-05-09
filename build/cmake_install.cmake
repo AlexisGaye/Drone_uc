e1195bc2000426bd4c0b1766872dfc8ff00de88c
lcy <253154143@qq.com>
2024-05-09 11:13:17 +0800

    cmake error is fixed

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/alexis/Project/uCOS_Drone
+# Install script for directory: /home/alexis/Project/uCOS_Drone (another copy)
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/alexis/Project/uCOS_Drone/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/alexis/Project/uCOS_Drone (another copy)/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
